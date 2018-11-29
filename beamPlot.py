5338e44f591aa72aa58d6389de643ba2d17fd637
elitenooben <44467760+elitenooben@users.noreply.github.com>
2018-11-29 14:42:01 +0100

    Add files via upload

--- beamPlot.py
+++ beamPlot.py
@@ -14,6 +14,7 @@ from decimal import Decimal
 def beamPlot(beamLength, loadPositions, loadForces, beamSupport):
     x_plot = np.arange(beamLength*1.001, step = beamLength/100)
     y_plot = -beamSuperposition(x_plot, beamLength, loadPositions, loadForces, beamSupport)
+    beamSupport = beamSupport.capitalize()
     
     #Negative beam deflection
     y_a = -beamSuperposition(a,l,a,W,beamSupport)
@@ -22,27 +23,25 @@ def beamPlot(beamLength, loadPositions, loadForces, beamSupport):
     plt.scatter(a, y_a, color = "r", s = 100, label = "Load Pos.")
     plt.scatter(x_plot[np.where(abs(y_plot) == max(abs(y_plot)))], max(y_plot, key = abs), color = "g", s = 100, label = "Defl_max")
     
-    plt.title("Beam Deflection\nBeamType: %s" %beamSupport.capitalize())
+    plt.title("Beam Deflection\nBeamType: %s" %beamSupport)
     plt.xlabel("Horizontal Length in [m]")
     plt.ylabel("Deflection from y_0 in [m]")
     for i in range(len(y_a)):
-        if beamSupport.capitalize() == "Both":
-            if a[i] < (beamLength/2)+1:
-                plt.text(a[i]-(1.5/14)*beamLength,y_a[i]+(0.5/14)*max(y_plot,key=abs), str(W[i]) + " N")
-            else:
-                plt.text(a[i]+(0.5/14)*beamLength,y_a[i]-(0.5/14)*max(y_plot,key=abs), str(W[i]) + " N")
-        else:
-            plt.text(a[i]+(0.5/14)*beamLength,y_a[i]-(0.5/14)*max(y_plot,key=abs), str(W[i]) + " N")
-    plt.text(x_plot[np.where(y_plot == max(y_plot, key = abs))]+(0.4/14)*beamLength,max(y_plot, key = abs), "%.2E m" % Decimal(str(max(y_plot, key = abs))))    
+        plt.text(a[i]+(0.5/14)*beamLength,y_a[i]-(0.5/14)*max(y_plot,key=abs), str(W[i]) + " N")
+    if beamSupport == "Cantilever":
+        plt.text(x_plot[np.where(y_plot == max(y_plot, key = abs))]+(0.4/14)*beamLength,max(y_plot, key = abs), "%.2E m" % Decimal(str(max(y_plot, key = abs))))  
+    else:
+        plt.text(x_plot[np.where(y_plot == max(y_plot, key = abs))]+(0.4/14)*beamLength,max(y_plot, key = abs)*1.04, "%.2E m" % Decimal(str(max(y_plot, key = abs))))  
     style.use("ggplot")
     plt.legend()
     
     plt.show()
-    return
+    return 
+
 
 l = 20
-a = np.array([10,17,5,11])
-W = np.array([-2,2000,13,500])
-beamSupport = "cantilever"
+a = np.array([10,3,17,6])
+W = np.array([200,150,240,560])
+beamSupport = "both"
 
 print(beamPlot(l,a, W, beamSupport))

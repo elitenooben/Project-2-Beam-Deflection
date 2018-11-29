4d0e9aa82d163730ac8e343900f51b69ce3b00d1
Carl Frederik Grønvald <bonezaux@gmail.com>
2018-11-29 13:47:38 +0100

    Finish loadFile and saveToFile

--- loadFile.py
+++ loadFile.py
@@ -10,21 +10,44 @@ import pandas as pd
 import os
 
 def loadFile():
+    filename = None
     while True:
         filename = input("Name of file to load (write 'I surrender' to quit): ")
-        if os.path.exists(filename+".csv") or os.path.exists(filename):
-            data = list(pd.read_csv(filename+".csv"))
+        if not filename.isalnum():
+            print("Write only alphanumerical characters :)")
+        elif os.path.exists(filename+".csv") or os.path.exists(filename):
             break
         elif filename.lower() == "i surrender":
-            return "Better luck next time"
+            print("Better luck next time!")
+            return 
         else:
             print("File not found. Try again.")
-    del data[-1]
-    beamLength = float(data[0])
-    beamSupport = data[1]
-    split = int(2+(len(data)-2)/2)
-    loadPositions = data[2:split]
-    loadForces = data[split:]
-    return loadForces
-
+            
+    #Load data from file using pandas
+    data = None
+    try:
+        with open(filename+".csv") as file:
+            data = pd.read_csv(file)
+            data = data.values.reshape(-1, 2)
+            beamLength = float(data[0,0])
+            beamSupport = data[0,1]
+            loadPositions = data[1:,0].astype(float)
+            loadForces = data[1:,1].astype(float)
+            
+            #Check whether supports are valid
+            if beamSupport != "Both" and beamSupport != "Cantilever":
+                print("Invalid beam support!")
+                return
+            
+            print("Loaded " + filename + ".csv!");
+            return [beamLength, beamSupport, loadPositions, loadForces]
+        print("Something went wrong with the io. Please retry")
+        return
+    
+    #If there is some error in there
+    except ValueError:
+        print("Some value in the file is invalid. Please only load files saved by this program.")
+    except:
+        print("Something very weird happened! Please only load files saved by this program.")
+    
 print(loadFile())
--- saveToFile.py
+++ saveToFile.py
@@ -8,28 +8,26 @@ Created on Thu Nov 22 15:06:20 2018
 import numpy as np
 import pandas as pd
 
-beamLength = 20
-beamSupport = "Both"
-loadPositions = np.array([5,11,17,15,0,17])
-loadForces = np.array([1000,430,17,9,8,7])
 
-
-def saveToFile():
-    safechars = "abcdefghijklmnopqrstuvwxyzæøå1234567890 -_."
+def saveToFile(beamLength, beamSupport, loadPositions, loadForces):
+    #Make sure there is something to save
     try:
         print("You are saving the following:\n\nBeamlength: %s\nBeamSupport: %s\nLoad position(s): %s\nForce(s) of load(s): %s" % (beamLength, beamSupport, loadPositions, loadForces))
     except NameError:
         return print("One must walk before one can fly. Please choose some values before trying to save.")
-        
+    
+    #Get a filename, save to filename.csv
     while True:
         filename = input("Please type the desired filename: ")
-        if len(set(filename.lower()+safechars)) != len(safechars):
-            print("please choose a name without weird characters :)")
+        if not filename.isalnum():
+            print("please choose a name consisting of alphanumerical characters :)")
         else:
-            values = [beamLength, beamSupport] + list(loadPositions) + list(loadForces)
+            #Create a matrix of values, 0th row is beam, rest are the loads.
+            values = np.array([beamLength, beamSupport])
+            for loadPosition, loadForce in zip(list(loadPositions), list(loadForces)):
+                values = np.vstack((values, np.array([loadPosition, loadForce])))
+            
             with open(filename+".csv", "w+") as f:
-                for item in values:
-                    f.write("%s," % item)
-            return print("Your file was saved as '" + filename + ".csv'. Good job! :D")
-                         
-saveToFile()
+                pd.DataFrame(values).to_csv(f, index=False)
+                print("Your file was saved as '" + filename + ".csv'. Good job! :D")
+            return
